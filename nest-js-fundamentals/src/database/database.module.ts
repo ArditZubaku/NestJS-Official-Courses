3bf6ff5860b5a67def1c20094dc86e6534d3187e
Ardit Zubaku <zubaku92@gmail.com>
2024-01-14 07:34:55 +0100

    Creating dynamic modules.

---
+++ nest-js-fundamentals/src/database/database.module.ts
@@ -0,0 +1,31 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+@Module({})
+// {
+//     providers: [
+//       {
+//         provide: 'CONNECTION',
+//         useValue: new DataSource({
+//           type: 'postgres',
+//           host: 'localhost',
+//           port: 5555,
+//         }).initialize(),
+//       },
+//     ],
+//   }
+export class DatabaseModule {
+  static register(options: DataSourceOptions): DynamicModule {
+    return {
+      module: DatabaseModule,
+      providers: [
+        {
+          provide: 'CONNECTION',
+          useValue: new DataSource({
+            ...options,
+          }).initialize(),
+        },
+      ],
+    };
+  }
+}
