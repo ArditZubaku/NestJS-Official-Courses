9cf6459da6a47799108763ea42e8c1ee10f6bc70
Ardit Zubaku <zubaku92@gmail.com>
2024-01-15 18:22:51 +0100

    Diving deeper into request-scoped providers.

--- nest-js-fundamentals/src/coffees/coffees.controller.ts
+++ nest-js-fundamentals/src/coffees/coffees.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  Inject,
   Param,
   ParseIntPipe,
   Patch,
@@ -13,10 +14,22 @@ import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDTO } from './dto/create-coffee.dto.ts/create-coffee.dto';
 import { UpdateCoffeeDTO } from './dto/update-coffee.dto/update-coffee.dto';
 import { PaginationQueryDTO } from '../common/dto/pagination-query.dto/pagination-query.dto';
+import { log } from 'console';
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
 
 @Controller('coffees')
 export class CoffeesController {
-  constructor(private readonly coffeesService: CoffeesService) {}
+  constructor(
+    private readonly coffeesService: CoffeesService,
+    //Injecting the original request object
+    @Inject(REQUEST) private readonly request: Request,
+  ) {
+    log(
+      'Even though the CoffeesController seems to be a Singleton since nothing seems to tell different, since it depends on the CoffeesService, which is a REQUEST scoped service, it will be REQUEST scoped as well.',
+    );
+    log('CoffeesController instantiated');
+  }
 
   @Get('flavours')
   //   findAll(@Res() response) {
@@ -26,6 +39,7 @@ export class CoffeesController {
     console.log(paginationQuery);
     // const { limit, offset } = paginationQuery;
     // return 'Returns all where. Limit is ' + limit + ' and offset is ' + offset;
+    log(this.request);
     return this.coffeesService.findAll(paginationQuery);
   }
 
