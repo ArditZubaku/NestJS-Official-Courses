3d2c054db70f54abeb8cf26bda51d0eb08004c6e
Ardit Zubaku <zubaku92@gmail.com>
2024-01-21 05:13:18 +0100

    Asynchronously configuring dynamic modules.

--- nest-js-fundamentals/src/app.module.ts
+++ nest-js-fundamentals/src/app.module.ts
@@ -5,12 +5,33 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
 import appConfig from './config/app.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        // type: 'postgres',
+        // host: process.env.DATABASE_HOST,
+        // port: +process.env.DATABASE_PORT, // + is for type conversion to number
+        // username: process.env.DATABASE_USER,
+        // password: process.env.DATABASE_PASSWORD,
+        // database: process.env.DATABASE_NAME,
+        // autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
+        // synchronize: true, // it will auto create tables based on the entities
+        type: 'postgres',
+        host: configService.getOrThrow('DATABASE_HOST'),
+        port: configService.getOrThrow('DATABASE_PORT'),
+        username: configService.getOrThrow('DATABASE_USER'),
+        password: configService.getOrThrow('DATABASE_PASSWORD'),
+        database: configService.getOrThrow('DATABASE_NAME'),
+        autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
+        synchronize: true, // it will auto create tables based on the entities
+      }),
+    }),
     ConfigModule.forRoot({
       // ignoreEnvFile: true,
       validationSchema: Joi.object({
@@ -20,16 +41,6 @@ import appConfig from './config/app.config';
       load: [appConfig],
     }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
     CoffeeRatingModule,
     DatabaseModule,
   ],
--- nest-js-fundamentals/src/main.ts
+++ nest-js-fundamentals/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { log } from 'console';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.useGlobalPipes(
     new ValidationPipe({
       enableDebugMessages: true,
@@ -16,6 +19,12 @@ async function bootstrap() {
       },
     }),
   );
-  await app.listen(3000);
+  // await app.listen(3000);
+  await app.listen(configService.getOrThrow('PORT'));
+  log(
+    `App is listening on port ${configService.getOrThrow<number>(
+      'PORT',
+    )} which is of type ${typeof configService.getOrThrow<number>('PORT')}`,
+  );
 }
 bootstrap();
