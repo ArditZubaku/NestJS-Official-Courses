ef26d10671984dbc0e73c33e85fef320879063df
Ardit Zubaku <zubaku92@gmail.com>
2024-01-24 06:05:40 +0100

    First e2e test.

--- nest-js-fundamentals/test/app.e2e-spec.ts
+++ nest-js-fundamentals/test/app.e2e-spec.ts
@@ -1,24 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpServer, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { ConfigService } from '@nestjs/config';
+import { IncomingHttpHeaders } from 'http';
+
+type Headers = {
+  [K in keyof IncomingHttpHeaders]: string;
+};
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let httpServer: HttpServer;
+  let configService: ConfigService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    httpServer = app.getHttpServer();
+    configService = app.get<ConfigService>(ConfigService);
     await app.init();
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    const headers: Headers = {
+      authorization: configService.getOrThrow('API_KEY'),
+    };
+
+    return request(httpServer)
       .get('/')
+      .set(headers)
       .expect(200)
       .expect('Hello World!');
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
