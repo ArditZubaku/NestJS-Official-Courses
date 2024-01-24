ab5a103f9db828520564faaf1f8978190ecf2f72
Ardit Zubaku <zubaku92@gmail.com>
2024-01-24 04:25:42 +0100

    Adding additional information using Swagger module's decorators.

--- nest-js-fundamentals/src/coffees/dto/create-coffee.dto.ts
+++ nest-js-fundamentals/src/coffees/dto/create-coffee.dto.ts
@@ -1,13 +1,17 @@
 import { IsString } from 'class-validator';
 import { Flavour } from '../entities/flavour.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCoffeeDTO {
+  @ApiProperty({ description: 'The name of a coffee.' })
   @IsString()
   readonly name: string;
 
+  @ApiProperty({ description: 'The brand of a coffee.' })
   @IsString()
   readonly brand: string;
 
+  @ApiProperty({ description: 'The flavours of a coffee.' })
   @IsString({ each: true })
   readonly flavours: Flavour[];
 }
